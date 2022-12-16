37f6e8422732c42da8ad68ee7a8104afbd16b35c
tyrellbain <tyrellbaindev@gmail.com>
2022-12-16 07:22:53 -0500

    style(Global): Rename StyledH2 to a more verbose name. Add shared styling for card details

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,10 +16,25 @@ export const PageHeader = styled.div`
   width: 100%;
 `;
 
-export const StyledH2 = styled.h2`
+export const CardTitle = styled.h2`
   color: ${(props) => props.theme.colors.text};
   font-size: 18px;
   font-weight: 600;
   line-height: 18px;
   margin-bottom: 4px;
+`;
+
+export const CardDetailsTitle = styled.h4`
+  color: ${(props) => props.theme.colors.secondaryText};
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 12px;
+  margin-bottom: ${(props) => props.theme.spacing.sm};
+`;
+
+export const CardDetailsValue = styled.p`
+  color: ${(props) => props.theme.colors.text};
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
 `;
